24134a6d843aa7ade067bfc668dd3fc4da116fae
CriticalGamer <upadhyaykavy@gmail.com>
2021-07-30 01:50:56 +0530

    all code together

---
+++ all.py
@@ -0,0 +1,83 @@
+import selenium.webdriver
+
+driver = selenium.webdriver.Firefox(executable_path='/Users/anonymous/Desktop/zoom-whatsapp-adhesive/geckodriver')
+driver.get("http://web.whatsapp.com")
+input()
+
+elements = driver.find_elements_by_class_name("_22Msk")
+print(len(elements))
+
+messages=[]
+for i in range(len(elements)):
+    print(elements[i].text)
+    messages.append(elements[len(elements)-i-1].text) 
+    input()
+
+#print(messages)
+info=[]
+def get_content(message):
+    msg=message.split()
+    #print(msg)
+    
+    i=0
+    name=''
+    while (msg[i] != "Join" and msg[i] != "is" ):
+        name=name+msg[i]+' '
+        if msg[i] == "Ma'am":
+            break
+        i+=1
+        if i >= len(msg):
+            break
+        
+    print("Name:", name)
+    
+    
+    i=0
+    time=''
+    while(msg[i] != 'Time:' ):
+        i+=1
+        if i >= len(msg):
+            break
+    i+=1        
+    if i < len(msg):
+                       
+        while msg[i] != 'Join' :
+            time= time+ msg[i] + ' '
+            if msg[i]=='India' :
+                break
+            i+=1
+            if i >= len(msg):
+                break
+            
+    print("time:", time)
+    
+    
+    
+    i=0
+    link=''
+    has_link=False
+    
+    while(i < len(msg)):
+        if (msg[i].startswith('https')):
+            has_link=True
+            link=msg[i]
+        i+=1    
+        
+    print("link:", link)
+        
+    
+        
+    print("\n")
+    '''{"teacher": get_teacher(message),
+                 "link" : get_link(message),
+                 "time_meet" : get_time(message),
+                 "id" : get_id(message),
+                 "pass" : get_pass(message)
+                }'''
+def update_info(messages):
+    info=[]
+    for message in messages:
+        get_content(message)
+
+        
+update_info(messages)
